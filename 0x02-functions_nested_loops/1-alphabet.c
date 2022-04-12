226fc5fbd18b547f82e04d79ada1918ad2d4de61
allyskorsah <whitneykorsah@gmail.com>
2022-04-12 16:38:33 -0700

    started nesting and loops task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include" main.h"
+
+/*
+ * main prints _putchar
+ * return 0 is success
+ */
+
+void print_alphabet(void);
+{
+char alpha = 'a';
+while (alpha <= 'z')
+	{_putchar(alpha);
+	alpha++;
+	}
+_putchar('\n')
+return (0);
+}
