9260386f8b29528f3ca5d0f10445ff73ead9f470
allyskorsah <whitneykorsah@gmail.com>
2022-04-12 17:44:33 -0700

    started nesting and loops task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,23 @@
 #include "main.h"
-/*
- * main prints _putchar
- * return 0 is success
+
+/**
+ * main - prints _putchar
+ *
+ * Description: prints _putchar
+ *
+ * Return: 0 (Success)
  */
+
 int main(void)
-{_putchar ("_putchar" \n);
-return (0);
+{
+	char text[9] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar('\n');
+
+	return (0);
 }
