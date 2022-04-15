f1a39acd34fa7a77ab488a8e96522293d507a517
allyskorsah <whitneykorsah@gmail.com>
2022-04-14 20:34:33 -0700

    started more fuctions, more loops in c task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ *_isupper - checks uppercase character
+ *@c: the character that is to be checked
+ *return: 1 if c is uppercase, 0 if its not
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z'); 
+}
