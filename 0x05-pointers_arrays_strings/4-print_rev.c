5c30961c5b84c8a113b3ae650a666b97db55f5c0
allyskorsah <whitneykorsah@gmail.com>
2022-04-20 15:04:18 -0700

    starting pointers, arrays and strings 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: characters
+ *
+ * Return: nothing
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	while (i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
