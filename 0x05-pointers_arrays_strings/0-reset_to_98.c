31d88bc394f6edb12535511d4390e4d60c72236f
allyskorsah <whitneykorsah@gmail.com>
2022-04-20 14:38:40 -0700

    starting pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - change argument to 98
+ * @n: integer
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+task 0x05 on C pointers, arrays and strings
