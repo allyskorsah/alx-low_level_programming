e5e56104a1d64b0ed545c686e2feec317a9e415b
allyskorsah <whitneykorsah@gmail.com>
2022-05-04 18:58:45 -0700

    starting project on argc argv 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -12,15 +12,11 @@
 
 int main(int argc, char *argv[])
 {
-	int i, j, sum;
-	sum = 0;
-
-	if (argc < 1)
-		printf("0\n");
+	int i, j, add = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		for (j = 0; argv[i][j]; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
@@ -28,8 +24,8 @@ int main(int argc, char *argv[])
 				return (1);
 			}
 		}
-		sum += atoi(argv[i]);
+		add += atoi(argv[i]);
 	}
-	printf("%d\n", sum);
+	printf("%d\n", add);
 	return (0);
 }
